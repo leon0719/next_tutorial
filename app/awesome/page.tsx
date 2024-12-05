274c47a644de9663ec757870f46506d240144c15
leon.huang <leon.huang@riversoft.com.tw>
2024-12-05 15:17:16 +0800

    feat: modify data folder

---
+++ app/awesome/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page: React.FC = () => {
+  return (
+    <main>NextJS is awesome!</main>
+  )
+}
+
+export default page
---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+import './globals.css'
+
+export const metadata = {
+  title: 'NextJS Course App',
+  description: 'Your first NextJS app!',
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
