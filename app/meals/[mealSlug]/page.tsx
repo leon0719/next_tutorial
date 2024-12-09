475a890b509f1f8ef456dc2c3253ccdf27e6f3b4
leon.huang <leon.huang@riversoft.com.tw>
2024-12-09 16:34:51 +0800

    feat: react tutorial 115

--- app/meals/[mealSlug]/page.tsx
+++ app/meals/[mealSlug]/page.tsx
@@ -1,14 +1,46 @@
-import Link from "next/link";
+import Image from "next/image";
+import classes from "./page.module.css";
 import React from "react";
+import { getMeal, Meal } from "@/lib/meals";
+import { notFound } from "next/navigation"; // 自動導向not found page
 
-function MealDetailsPage() {
+interface MealProps {
+  params: {
+    mealSlug: number;
+  };
+}
+
+async function MealDetailsPage({ params }: MealProps) {
+  const meal: Meal = await getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
-    <div>
-      <h1>Meal</h1>
-      <p>
-        <Link href="/meals">Back</Link>
-      </p>
-    </div>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill src={meal.image} alt={meal.title} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${"EMAIL"}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
 
--- lib/meals.ts
+++ lib/meals.ts
@@ -1,15 +1,37 @@
 import sql from "better-sqlite3";
 
+export interface Meal {
+  image: string;
+  title: string;
+  creator: string;
+  summary: string;
+  instructions: string;
+  slug: number;
+}
+
 const db = sql("meals.db");
 
-export async function getMeals() {
+export async function getMeals(): Promise<Meal[]> {
   await new Promise((resolve) => {
     setTimeout(() => {
       resolve(true);
     }, 2000);
   });
-  const meals = db.prepare(`SELECT * FROM meals`).all(); // .run() 是如果要插入數據或更改數據
 
-  // throw new Error("Something went wrong");
+  // Type the query result as Meal[]
+  const meals = db.prepare(`SELECT * FROM meals`).all() as Meal[];
   return meals;
+}
+
+export async function getMeal(slug: number): Promise<Meal> {
+  // Type the query result as Meal
+  const meal = db
+    .prepare(`SELECT * FROM meals WHERE slug = ?`)
+    .get(slug) as Meal;
+
+  if (!meal) {
+    throw new Error(`Meal with slug ${slug} not found`);
+  }
+
+  return meal;
 }
